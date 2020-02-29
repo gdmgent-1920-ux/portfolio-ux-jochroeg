f499bfdcc5ba9170c632144cd629afce04e33460
jochroeg <jochroeg@student.arteveldehs.be>
2020-02-29 17:12:22 +0100

    Update files assgnment1
    This file contains:
    
    - adobe XD file
    - PDF Inspiration
    - Shared adobe XD link

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/51a0f923-a597-4e94-7dc0-ba0f887a5193-2791/",
                 "target": "_blank"
             }
         ]
